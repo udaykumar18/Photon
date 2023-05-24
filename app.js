ed29d2afc748a018366bf6c62c6f8e91e25e46e6
udaykumar18 <114995146+udaykumar18@users.noreply.github.com>
2023-05-24 23:00:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const auth = "CfcnVg8v02wsv66KUBLVvU63Oo23haJ8ZZn4mYRU54IKxiItvKhJv0G0";
+
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".search-form");
+
+const more = document.querySelector(".more");
+
+let page = 1;
+let fetchLink;
+let currentSearch;
+
+let searchValue;
+//Event Listeners
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  currentSearch = searchValue;
+  searchPhotos(searchValue);
+});
+
+more.addEventListener("click", loadMore);
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
+  const data = await dataFetch.json();
+  return data;
+}
+
+function generatePictures(data) {
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `
+        <div class="gallery-info">
+        <p>${photo.photographer}</p>
+        <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}></img>`;
+    gallery.append(galleryImg);
+  });
+}
+
+async function curatedPhotos() {
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
+}
+
+async function searchPhotos(search) {
+  clear();
+  fetchLink = `https://api.pexels.com/v1/search?query=${search}&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
+}
+
+async function loadMore() {
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
+}
+
+curatedPhotos();
